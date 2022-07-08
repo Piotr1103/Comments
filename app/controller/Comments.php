20093c037db887eb388064ea234a1a4dee1dbf83
Piotr1103 <manjudamin@gmail.com>
2022-07-08 10:29:07 +0800

    First publish

---
+++ app/controller/Comments.php
@@ -0,0 +1,67 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller;
+
+use think\Request;
+use app\model\Comments as CommentsModel;
+
+class Comments
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+        return CommentsModel::select();
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+        return CommentsModel::where('cid', $id)->select();
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
